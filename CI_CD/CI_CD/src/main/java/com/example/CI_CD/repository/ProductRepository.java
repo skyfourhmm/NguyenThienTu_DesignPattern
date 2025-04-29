6dd8b362331d4d1d34bb7fb8ad3cb9d4f756a0a0
Thiên Tứ <nguyenthientu413@gmail.com>
2025-04-29 16:36:59 +0700

    Delete CI_CD/CI_CD directory

--- CI_CD/CI_CD/src/main/java/com/example/CI_CD/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.iuh.fit.dhktpm17a.springbootrenderdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.iuh.fit.dhktpm17a.springbootrenderdemo.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
